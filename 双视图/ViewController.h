f323f780a2bd546266ef858f0c98843a823db4e0
刘杨 <liuyang14258@qq.com>
2013-11-21 15:47:10 +0800

    Revert "抽屉视图"
    This reverts commit 6af567277c62fd6ace8bf510ecf15a0b37b34a18.

--- 双视图/ViewController.h
+++ 双视图/ViewController.h
@@ -7,11 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+
 @interface ViewController : UIViewController
-{
-    UIWindow *window;
-    UIView *myView;
-    AppDelegate *app;
-}
+
 @end
--- 双视图/ViewController.m
+++ 双视图/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "AppDelegate.h"
 
 @interface ViewController ()
 
@@ -19,45 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    window = [[UIWindow alloc]initWithFrame:CGRectMake(321, 0, 320, 460)];
-
-    myView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 460)];
-    
-    [myView setBackgroundColor:[UIColor redColor]];
-    myView.alpha = 0.7;
-    
-    [window addSubview:myView];
-    
-    [window makeKeyAndVisible];
-    [self.view addSubview:window];
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [button setTitle:@"切换" forState:UIControlStateNormal];
-
-    [button setFrame:CGRectMake(30, 30, 40, 20)];
-    [button addTarget:self action:@selector(chageView) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
-}
-
--(void)chageView
-{
-    [UIView animateWithDuration:0.5 animations:^{
-        [window setFrame:CGRectMake(0, 0, 320, 460)];
-    }];
-    
-    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(30, 60, 42, 20)];
-    [button setBackgroundColor:[UIColor blackColor]];
-    [myView addSubview:button];
-    
-    [button addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
-}
-
--(void)goBack
-{
-    [UIView animateWithDuration:0.5 animations:^{
-        [myView setFrame:CGRectMake(321, 0, 320, 460)];
-    }];
 }
 
 - (void)didReceiveMemoryWarning
